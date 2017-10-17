27037359a7ce7126731a1dcae8628a81c5bbb477
Breno Henrique <brenosc2@hotmail.com>
2017-10-17 21:46:45 -0200

    initial flow and new sendLink func

--- index.js
+++ index.js
@@ -1,33 +1,31 @@
-/*
-  CONGRATULATIONS on creating your first Botpress bot!
+const sendLink = (event, link) => {
+  event.reply('#send-link', { link: `${link}` })
+}
 
-  This is the programmatic entry point of your bot.
-  Your bot's logic resides here.
-  
-  Here's the next steps for you:
-  1. Read this file to understand how this simple bot works
-  2. Read the `content.yml` file to understand how messages are sent
-  3. Install a connector module (Facebook Messenger and/or Slack)
-  4. Customize your bot!
-
-  Happy bot building!
-
-  The Botpress Team
-  ----
-  Getting Started (Youtube Video): https://www.youtube.com/watch?v=HTpUmDz9kRY
-  Documentation: https://botpress.io/docs
-  Our Slack Community: https://slack.botpress.io
-*/
+const LINKS = {
+  presentation: 'asdasd',
+  repo: 'repo',
+  www: 'www.brenohq.com'
+}
 
 module.exports = function (bp) {
   bp.hear(/GET_STARTED|ola|oi|teste|hello|hi/i, (event, next) => {
     event.reply('#welcome')
-      .then(() => {
-        event.reply('#welcome')
-      })
+  })
+
+  bp.hear(/1|um/i, (event, next) => {
+    sendLink(event, LINKS.presentation)
+  })
+
+  bp.hear(/2|dois/i, (event, next) => {
+    sendLink(event, LINKS.repo)
+  })
+
+  bp.hear(/3|tres/i, (event, next) => {
+    sendLink(event, LINKS.www)
   })
 
   bp.hear(/exit|bye|goodbye|quit|done|leave|stop/i, (event, next) => {
-    event.reply('#goodbye', { reason: 'asdasdasd' })
+    event.reply('#goodbye', { reason: 'palestra chata' })
   })
 }
