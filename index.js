31bef302a15c0d6575fe7caf3093b90c27017209
Breno Henrique <brenosc2@hotmail.com>
2017-10-17 22:32:04 -0200

    sending template buttons

--- index.js
+++ index.js
@@ -1,31 +1,11 @@
-const sendLink = (event, link) => {
-  event.reply('#send-link', { link: `${link}` })
-}
-
-const LINKS = {
-  presentation: 'asdasd',
-  repo: 'repo',
-  www: 'www.brenohq.com'
-}
+const randomNumber = () => Math.floor(Math.random() * 50)
 
 module.exports = function (bp) {
   bp.hear(/GET_STARTED|ola|oi|teste|hello|hi/i, (event, next) => {
-    event.reply('#welcome')
+    event.reply('#send-links')
   })
 
-  bp.hear(/1|um/i, (event, next) => {
-    sendLink(event, LINKS.presentation)
-  })
-
-  bp.hear(/2|dois/i, (event, next) => {
-    sendLink(event, LINKS.repo)
-  })
-
-  bp.hear(/3|tres/i, (event, next) => {
-    sendLink(event, LINKS.www)
-  })
-
-  bp.hear(/exit|bye|goodbye|quit|done|leave|stop/i, (event, next) => {
-    event.reply('#goodbye', { reason: 'palestra chata' })
+  bp.hear(/tchau|xau|exit|bye|goodbye|quit|done|leave|stop/i, (event, next) => {
+    event.reply('#goodbye', { number: randomNumber() })
   })
 }
